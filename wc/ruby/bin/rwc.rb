48c95c66a86b087d62d3a3ff457d6ecb62a97096
kanhaya kumar yadav <kanhayaky120@gmail.com>
2023-09-27 00:45:41 +0530

    Step2: Count the lines in a file (option '-l')

--- wc/ruby/bin/rwc.rb
+++ wc/ruby/bin/rwc.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 def get_file_data(file_path)
-  file_data = File.read(file_path)
+  file_data = File.read(file_path, :encoding => 'utf-8')
   return file_data
 end
 
@@ -13,6 +13,10 @@ end
 # puts count_bytes("") # should return 0
 # puts count_bytes("hello world") # should return 11
 
+def count_lines(data="")
+  data.split("\n").length
+end
+
 
 option, file_path, *args = ARGV
 
@@ -22,6 +26,9 @@ case option
 when '-c'
   puts "Counting the bytes in the file"
   puts count_bytes(file_data)
+when '-l'
+  puts "Counting the lines in the file"
+  puts count_lines(file_data)
 else
   puts "Invalid option"
 end
